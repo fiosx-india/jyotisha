6a818caec84a6ededa6fb1b8a5c415fddfa4b4a6
fiosx-india <sharvikaenterprises2015@gmail.com>
2026-09-09 21:54:25 +0530

    Add test message to Streamlit app
    Added a test message to the Streamlit app.

--- app.py
+++ app.py
@@ -3,6 +3,9 @@
 ஸ்ட்ரீம்லிட் (Streamlit) அடிப்படையிலான முழுமையான ஜோதிட கணிப்பு செயலி
 """
 import streamlit as st
+st.write("Hello World - Test")
+
+import streamlit as st
 import pandas as pd
 from datetime import datetime, date, time
 from PIL import Image
